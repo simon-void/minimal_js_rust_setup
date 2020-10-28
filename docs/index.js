e83e00566f3ad6ea92ca081fcd75368a5f4c244e
StephanS <simon.void+github@gmail.com>
2020-10-28 13:56:19 +0000

    fixed set_text

--- docs/index.js
+++ docs/index.js
@@ -4,14 +4,15 @@ async function display_greeting() {
     console.log("init wasm")
     await wasm.init()
     console.log("invoking wasm")
-    const greeting = await wasm.get_greeting_for("Mike")
+    let greeting = await wasm.get_greeting_for("Mike")
     console.log("received from wasm: " + greeting)
     set_text(greeting)
 }
 
 function set_text(text) {
     let msg_div=document.getElementById("msg_box")
-    msg_div.innerText = greeting
+    msg_div.innerText = text
+    console.log("set text to: " + text)
 }
 
 console.log("executing index.js")
